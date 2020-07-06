fb4bebba6e4c34f6f3202a08675863930f9ad059
Natalia <natalia.kiwibird@gmail.com>
2020-07-06 17:33:32 +0100

    thank-you-500: setTimeout fixed

--- Thank you 500/thank-you-500.js
+++ Thank you 500/thank-you-500.js
@@ -1,18 +1,20 @@
 const counter = document.querySelector('.counter');
 const thx = document.querySelector('.thx');
-const span = document.querySelector('span');
 
 
 let output = 0;
-const counting = setInterval(() => {
-    counter.textContent = `${output}`;
-    if(output === 500){
-        counter.textContent = `+ ${output}`
-        thx.style.opacity = "1";
-    } else {
-        setTimeout(counting, 2000);
-        output++;
-    }
-}, 10);
+
+const delay = 
+    setTimeout(() => {
+        const counting = setInterval(() => {
+            counter.textContent = `${output}`;
+            if(output === 500){
+                counter.textContent = `+ ${output}`
+                thx.style.opacity = "1";
+            } else {
+                output++;
+            }
+        }, 10);
+    }, 2000);
 
 
